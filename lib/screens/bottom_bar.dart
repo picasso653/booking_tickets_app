8a3e76c6c633749b5cb2d067cc9ec46ca9af0bb0
Isaac Korankye Asante <xquisit31@gmail.com>
2022-11-01 16:31:24 +0000

    added the bottom navigation bar and started with the the home page

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/cupertino.dart';
+// ignore: unnecessary_import
+// ignore_for_file: prefer_const_constructors
+
+import 'package:booking_tickets_app/screens/home_screen.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
 class BottomBar extends StatefulWidget {
@@ -9,21 +13,57 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
+  int selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    const Text('Search'),
+    const Text('Tickets'),
+    const Text('Profile')
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My Tickets'),
-
+      appBar: AppBar(
+        title: Text('My Tickets'),
       ),
       body: Center(
-        child: Text('My body'),
+        child: _widgetOptions[0],
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-        BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket),label: 'Tickets'),
-        BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Profile'),
-      ] ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: _onItemTapped,
+          elevation: 10,
+          showSelectedLabels: false,
+          showUnselectedLabels: true,
+          selectedItemColor: Colors.white,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_home_add_regular),
+                backgroundColor: Colors.red,
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_home_add_filled),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+                backgroundColor: Colors.purple,
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+                label: 'Search'),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+                backgroundColor: Colors.deepPurple,
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+                label: 'Tickets'),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+                backgroundColor: Color.fromARGB(255, 3, 80, 143),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_person_add_filled),
+                label: 'Profile'),
+          ]),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          // ignore: avoid_unnecessary_containers
+          Container(
+            child: Column(
+              children: [
+                Row(
+                  children:  [
+                    // ignore: prefer_const_constructors
+                    Text('Good Morning'),
+                    Image.asset('asset/plane_1.jpg'),
+                  ],
+                )
+              ]),
+          )
+        ],
+      ),
+    );
+  }
+}
