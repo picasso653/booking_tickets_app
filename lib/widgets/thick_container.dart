44daab80d834437959a760f3812a424e31f19a08
Isaac Korankye Asante <xquisit31@gmail.com>
2022-11-06 21:19:49 +0000

    thick container

---
+++ lib/widgets/thick_container.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class ThickContainer extends StatelessWidget {
+  const ThickContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(3.0),
+      decoration: BoxDecoration( borderRadius: BorderRadius.circular(20),
+      border: Border.all(width: 2.5, color: Colors.white)),
+    );
+  }
+}
