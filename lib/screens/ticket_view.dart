f1d5d6c74f11f71fbcddb725cfc529ec545ed904
Isaac Korankye Asante <xquisit31@gmail.com>
2022-11-06 21:19:23 +0000

    started the tickets view

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,3 +1,5 @@
+import 'package:booking_tickets_app/utils/app_styles.dart';
+import 'package:booking_tickets_app/widgets/thick_container.dart';
 import 'package:flutter/material.dart';
 
 class TicketView extends StatelessWidget {
@@ -8,6 +10,32 @@ class TicketView extends StatelessWidget {
     return SizedBox(
       width: MediaQuery.of(context).size.width,
       height: 200,
+      child: Container(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(230, 51, 91, 223),
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(21),
+                topRight: Radius.circular(21)),
+              ),
+              padding: EdgeInsets.all(20),
+              child: Row(
+                children: [Text("NYC", style: Styles.headlineText3.copyWith(color: Colors.white),),
+                Expanded(child: Container()),
+                Expanded(child: Container()),
+                ThickContainer(),
+                ThickContainer(),
+                Expanded(child: Container()),
+                Text("London", style: Styles.headlineText3.copyWith(color: Colors.white),),
+                
+
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
